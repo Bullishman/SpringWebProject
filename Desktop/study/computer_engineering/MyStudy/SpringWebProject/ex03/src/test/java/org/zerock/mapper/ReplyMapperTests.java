5e22df67ed6a8ab38d28d4ae6d1fbb6c465e3841
Bullishman <deapark123@gmail.com>
2019-07-24 23:24:47 +0900

    Upload and Update

---
+++ Desktop/study/computer_engineering/MyStudy/SpringWebProject/ex03/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -0,0 +1,92 @@
+package org.zerock.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration({
+	"file:src/main/webapp/WEB-INF/spring/root-context.xml"
+})
+
+// Java Config
+//@ContextConfiguration(classes = { org.zerock.config.RootConfig.class })
+
+@Log4j
+public class ReplyMapperTests {
+	
+	private Long[] bnoArr = {207L,
+			206L,
+			205L,
+			204L,
+			203L};
+
+	@Setter(onMethod_ = @Autowired)
+	private ReplyMapper mapper;
+	
+	@Test
+	public void testMapper() {
+		log.info(mapper);
+	}
+	
+//	@Test
+//	public void testCreate() {
+//		IntStream.rangeClosed(1, 10).forEach(i -> {
+//			
+//			ReplyVO vo = new ReplyVO();
+//			
+//			// 掲示物の番号。
+//			vo.setBno(bnoArr[i%5]);
+//			vo.setReply("リプライ" + i);
+//			vo.setReplyer("replyer" + i);
+//			
+//			mapper.insert(vo);
+//		});
+//	}
+	
+	@Test
+	public void testRead() {
+		Long targetRno = 5L;
+		
+		ReplyVO vo = mapper.read(targetRno);
+		
+		log.info(vo);
+	}
+	
+	@Test
+	public void testDelete() {
+		Long targetRno = 1L;
+		
+		mapper.delete(targetRno);
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+		Long targetRno = 10L;
+		
+		ReplyVO vo = mapper.read(targetRno);
+		
+		vo.setReply("Update Reply");
+		
+		int count = mapper.update(vo);
+		
+		log.info("UPDATE COUNT: " + count);
+		
+	}
+	
+	@Test
+	public void testList() {
+		
+		Criteria cri = new Criteria();
+		
+	}
+	
+}
