93a6acdfc94dfaf6577ea6d6f7761725670bd7a4
Bullishman <deapark123@gmail.com>
2019-07-27 21:23:03 +0900

    Upload and Update

--- Desktop/study/computer_engineering/MyStudy/SpringWebProject/ex03/src/main/java/org/zerock/controller/ReplyController.java
+++ Desktop/study/computer_engineering/MyStudy/SpringWebProject/ex03/src/main/java/org/zerock/controller/ReplyController.java
@@ -1,19 +1,26 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
-@RequestMapping("/replies/*")
+@RequestMapping("/replies/")
 @RestController
 @Log4j
 @AllArgsConstructor
@@ -33,8 +40,67 @@ public class ReplyController {
 		
 		log.info("Reply INSERT COUNT: " + insertCount);
 		
-		
 		return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
+	
+	@GetMapping(
+			value = "pages/{bno}/{page}",
+			produces = {
+				MediaType.APPLICATION_JSON_UTF8_VALUE,
+				MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(
+			@PathVariable("page") int page,
+			@PathVariable("bno") Long bno) {
+		
+		log.info("getList............");
+		Criteria cri = new Criteria(page, 10);
+		
+		log.info("cri: " + cri);
+		
+		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+	}
+	
+	
+	@GetMapping(
+			value = "/{rno}",
+			produces = {
+				MediaType.APPLICATION_XML_VALUE,
+				MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+		
+		log.info("get: " + rno);
+		
+		return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+	}
+	
+	
+	@DeleteMapping(
+			value = "/{rno}",
+			produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+		
+		log.info("remove: " + rno);
+		
+		return service.remove(rno) == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
+	// 書き込みの修正は'PUT'方式や'PATCH'方式を利用して処理し、実際の修正されるデータはJSONフォーマットなので、@RequestBodyを利用して処理します、
+	// @RequestBodyで処理されるデータは一般パラメータや@PathVariableパラメータを処理できないため、直接処理してくれる部分を注意しなければなりません。
+	@RequestMapping(
+			method = { RequestMethod.PUT, RequestMethod.PATCH },
+			value = "/{rno}",
+			consumes = "application/json",
+			produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> modify(
+				@RequestBody ReplyVO vo,
+				@PathVariable("rno") Long rno) {
+		
+		vo.setRno(rno);
+		
+		log.info("rno: " + rno);
+		
+		return service.modify(vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
