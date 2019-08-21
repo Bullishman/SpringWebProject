612b44dfc659b4aedeb08c6ca33e7e4ef3c9b535
Bullishman <deapark123@gmail.com>
2019-08-21 23:42:14 +0900

    Upload and Update

---
+++ Desktop/study/computer_engineering/MyStudy/SpringWebProject/ex06/src/main/java/org/zerock/domain/AutoVO.java
@@ -0,0 +1,14 @@
+package org.zerock.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class AutoVO {
+
+	private String userid;
+	private String auth;
+	
+}
